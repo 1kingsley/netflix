da3187216315fbc590440884604c932fd5e7b2a9
1kingsley <you@example.com>
2022-05-02 21:56:15 +0530

    first commit

---
+++ netflix/src/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+function Navbar(){
+    return(
+        
+     <div className="nav">
+       <img className="nav_logo" src="https://www.logo.wine/a/logo/Netflix/Netflix-Logo.wine.svg" alt="Netflix Logo"></img>
+       <img className="nav_avatar" src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png" alt="User Logo"></img>
+     </div>
+     
+    )
+}
+
+export default Navbar;
